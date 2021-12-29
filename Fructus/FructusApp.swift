be136feba867af89eb7b6a6f5c09b47f91f77301
griends1 <sgriendt@gmail.com>
2021-12-28 20:52:57 -0800

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Sebastiaan van de Griendt on 28/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
